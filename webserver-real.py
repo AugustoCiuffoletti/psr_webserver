f37635303806c913177760b9badbc3eb421e7387
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2017-01-25 11:48:42 +0100

    Aggiunta pagina dinamica

--- webserver-real.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import SimpleHTTPServer, SocketServer
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", 80), Handler)
-print "Server pronto..."
-httpd.serve_forever()
